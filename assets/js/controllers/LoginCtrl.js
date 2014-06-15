80ba923a6aa4d5b5389d5915a2277571f74aabce
Andreas Møller <andreas.moller@gmail.com>
2014-06-15 21:01:26 +0100

    logout

--- assets/js/controllers/LoginCtrl.js
+++ assets/js/controllers/LoginCtrl.js
@@ -18,18 +18,17 @@ angular.module('app')
             $http.post('/users/login', $scope.credentials)
                 .success(function (response) {
 
-                  mixpanel.identify(response.id);
-                  mixpanel.people.set(angular.copy({
+                    mixpanel.identify(response.id);
+                    mixpanel.people.set(angular.copy(response,{
                       "$first_name": response.firstname,
                       "$last_name": response.lastname,
                       "$created": response.createdAt,
                       "$email": response.email
-                  }, response));
-                  mixpanel.track('Logged In');
-
-                    $location.path('/store');
+                    }));
+                    mixpanel.track('Logged In');
                     $scope.$root.showHeader = true;
                     $scope.$root.user = response;
+                    $location.path('/store');
                 })
                 .error(function (error) {
                     console.log('ERROR');
